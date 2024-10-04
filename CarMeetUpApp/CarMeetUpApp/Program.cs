116280b97e23eeb3dc3265d79f63a73f17940027
Ayow9 <alexander.yowell@gmail.com>
2024-10-03 21:21:43 -0400

    added auth0

--- CarMeetUpApp/CarMeetUpApp/Program.cs
+++ CarMeetUpApp/CarMeetUpApp/Program.cs
@@ -1,3 +1,6 @@
+using CarMeetUpApp;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+//AUTH ZERO
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = "https://dev-5w5l8ake23bk7o4h.us.auth0.com/";  // Your Auth0 domain
+    options.Audience = "https://recipe-api.com";  // Your Auth0 API identifier
+});
+
+//AUTH ZERO
+//This is so in swagger you can put the token in otherwise the endpoint
+//would be blocked. Check the static Swagger Extension class for more details
+builder.Services.AddCustomSwagger();
 
 var app = builder.Build();
 
@@ -16,10 +47,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
+
 app.UseHttpsRedirection();
 
+//AUTH ZERO 
+//make sure use authentication and use authorization are in this EXACT order
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
---
+++ CarMeetUpApp/CarMeetUpApp/SwaggerExt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CarMeetUpApp;
+
+public static class SwaggerExt
+{
+    public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Please enter the token (without 'Bearer' prefix) into the field below.",
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
+        });
+
+        return services;
+    }
+}
