96355987aa914467d028d6854692b9e4fc39d311
Freddie <Freddiex06@outlook.com>
2024-10-07 16:55:35 -0700

    Dbcontextupdate

---
+++ CarMeetUpApp/CarMeetUpApp/Data/CarMeetUpDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarMeetUpApp.Data;
+
+public class CarMeetUpDbContext : DbContext
+
+{
+
+    public CarMeetUpDbContext(DbContextOptions<CarMeetUpDbContext> options) : base(options)
+    {
+
+    }
+
+    //public DbSet<User> EventListing { get; set; }
+    //public DbSet<EventSignUp> EventSignUp { get; set; }
+}
