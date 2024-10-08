d64ba7934172b0be3d45d7a45f395b1932d5b0ef
Ayow9 <alexander.yowell@gmail.com>
2024-10-07 20:01:26 -0400

    added models, enumns

---
+++ CarMeetUpApp/CarMeetUpApp/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarMeetUpApp.Models;
+
+public class User
+{
+   [Key]
+   int UserId { get; set; }
+   [Required]
+   string FirstName { get; set; }
+   [Required]
+   string LastName { get; set; }
+   [Required]
+   string Email { get; set; }
+   [MaxLength(500)]
+   string Bio { get; set; }
+   [MaxLength(100)]
+   string CarInterests { get; set; }
+
+
+}
+
+
